35da6a80abea0b2089b7257e3e514f25d233d5ae
Rob Zwissler <rob@zwissler.org>
2018-02-14 15:01:25 -0800

    Makefile prep - moving requires out of module

--- lib/ghrep.rb
+++ lib/ghrep.rb
@@ -5,23 +5,22 @@
 # - Consider adding Slack notifications for pull request submission
 # - Consider adding pre-fetch/thread pool layer for blocking loops
 #
+require 'benchmark'
+require 'io/console'
+require 'pp'
+
+require_relative 'ghrep/parse_command_line_args.rb'
+require_relative 'ghrep/git_command.rb'
+require_relative 'ghrep/github.rb'
+require_relative 'ghrep/github_error_message.rb'
+require_relative 'ghrep/log.rb'
+require_relative 'ghrep/parse_command_line_args.rb'
+require_relative 'ghrep/parse_terms.rb'
+require_relative 'ghrep/repo.rb'
+require_relative 'ghrep/repo_spec.rb'
+require_relative 'ghrep/repos.rb'
+
 module Ghrep
-  require 'benchmark'
-  require 'io/console'
-  require 'pp'
-
-  #GHREP#
-  require_relative 'ghrep/parse_command_line_args.rb'
-  require_relative 'ghrep/git_command.rb'
-  require_relative 'ghrep/github.rb'
-  require_relative 'ghrep/github_error_message.rb'
-  require_relative 'ghrep/log.rb'
-  require_relative 'ghrep/parse_command_line_args.rb'
-  require_relative 'ghrep/parse_terms.rb'
-  require_relative 'ghrep/repo.rb'
-  require_relative 'ghrep/repo_spec.rb'
-  require_relative 'ghrep/repos.rb'
-
   # https://tools.ietf.org/html/rfc7231#section-6.1
   GITHUB_ERRORS = {
     401 => 'Unauthorized',
